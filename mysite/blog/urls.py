a13c53fb9d9c479c278c6e329e1f6877f31e85f6
Murali Suresh <muralisc@gmail.com>
2015-10-09 11:13:51 +0530

    new post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_new')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
