881c571c8da2f0d39e8b6a62edc624eaf87dd267
Murali Suresh <muralisc@gmail.com>
2015-10-09 12:06:00 +0530

    user auth

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^login/$', views.lin, name='login'),
+    url(r'^create/$', views.create_user, name='create'),
+    url(r'^logout/$', views.lout, name='logout'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,21 +2,56 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    if request.user.is_authenticated():
+        posts = Post.objects.all()
+        return render(request, 'blog/post_list.html', {'posts': posts})
+    else:
+        return redirect( 'blog.views.lin' )
 
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('blog.views.post_new')
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('blog.views.post_new')
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
     else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return redirect( 'blog.views.lin' )
+
+def create_user( request ):
+    if request.POST:
+        u = request.POST['username']
+        p = request.POST['password']
+        e = request.POST['email']
+        user = User.objects.create_user( u, e, p)
+        return redirect( 'blog.views.lin' )
+    return render(request, 'blog/create.html' )
+
+def lin(request):
+    if request.user.is_authenticated():
+        return redirect( 'blog.views.post_list' )
+    if request.POST:
+        print( request.POST )
+        u = request.POST['username']
+        p = request.POST['password']
+        user = authenticate( username=u, password=p )
+        if user is not None:
+            if user.is_active:
+                login( request, user )
+                return redirect('blog.views.post_list')
+    return render(request, 'blog/login.html', {})
+
+def lout( request ):
+    logout( request )
+    return redirect( 'blog.views.lin' )
